a9ae81eaa0c219879b16fd0b9285ebf329b93c6b
Prabhu-Acharya <prabhu.acharya16@gmail.com>
2026-04-04 23:03:31 +0530

    Adding next ques

--- Arrays_Questions/FirstUniqueCharacterInString.java
+++ Arrays_Questions/FirstUniqueCharacterInString.java
@@ -1,4 +1,27 @@
 import java.util.*;
 public class FirstUniqueCharacterInString {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter String:");
+        String s = sc.nextLine();
+
+        int result = firstUnqChar(s);
+
+        System.out.println("First Unique Character Index:" +result);
+    }
+    public static int firstUnqChar(String s){
+        int freq[] = new int[26];
+
+        for(int i =0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+        }
+
+        for(int i =0;i<s.length();i++){
+            if(freq[s.charAt(i)-'a']==1){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
