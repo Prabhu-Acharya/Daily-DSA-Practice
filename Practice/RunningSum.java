04bec332fd973a91a6c232a438b68f6b6e362363
Prabhu-Acharya <prabhu.acharya16@gmail.com>
2026-04-04 22:44:33 +0530

    Adding next ques

--- Practice/RunningSum.java
+++ Practice/RunningSum.java
@@ -1,3 +1,20 @@
+import java.util.*;
+
 public class RunningSum {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size:");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter elements:");
+        for(int i =0;i<n;i++){
+            nums[i] = sc.nextInt();
+        }
+
+        for(int i = 1;i<nums.length;i++){
+            nums[i] = nums[i] + nums[i-1];
+        }
+        System.out.println(Arrays.toString(nums));
+    }
 }
